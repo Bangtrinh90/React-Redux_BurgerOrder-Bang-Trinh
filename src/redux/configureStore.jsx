aa0a0fa3ab2bb10a12dddba2731f1c73c179806e
Bangtrinh90 <bangtrinhftu@gmail.com>
2022-12-10 15:16:55 +0700

    up 1

--- src/redux/configureStore.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { BurgerReducer } from "./reducers/BurgerReducer";
-
-export const store = configureStore({
-  reducer: {
-    BurgerReducer,
-  },
-});
